74714075942a16b853cb9a822d3599570c9c7852
Domas <domas.velykis@gmail.com>
2018-03-24 22:45:34 +0200

    added webpage prototype built with react

--- src/main/java/com/eshop/dao/ProductDao.java
+++ src/main/java/com/eshop/dao/ProductDao.java
@@ -16,6 +16,9 @@ public class ProductDao {
         put(1, new Product(1, "Laptop", 500.40));
         put(2, new Product(2, "Phone", 350.99));
         put(3, new Product(3, "Jacket", 54.95));
+        put(4, new Product(4, "CPU", 169.70));
+        put(5, new Product(5, "Hat", 14.95));
+        put(6, new Product(6, "Headphones", 84.99));
     }};
 
     public Collection<Product> getItems() {
