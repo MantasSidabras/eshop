650cdfbb5bf4a5f13b4c79fec73607f1b990c8d3
Karolis <incomirican@gmail.com>
2018-05-02 13:17:40 +0300

    Product DAO findByName

--- src/main/java/com/eshop/dao/ProductDAO.java
+++ src/main/java/com/eshop/dao/ProductDAO.java
@@ -4,7 +4,11 @@ import com.eshop.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductDAO extends JpaRepository<Product, Integer> {
     //Basic CRUD auto-implemented
+
+    List<Product> findByName(Integer id);
 }
