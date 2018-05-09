9d90421a74ef3e533f07968397a849e13bd4bec1
Paulius Sergeda <adegres@gmail.com>
2018-05-09 14:01:22 +0300

    modified:   src/main/java/com/eshop/dao/ProductDAO.java 		changed findbyName argument from Integer id to String Name
    	new file:   src/main/java/com/eshop/exceptions/InvalidProductQuantityException.java
    			if there's a request to add products to a cart, but there aren't enough
    			in storage, this will be thrown
    	modified:   src/main/java/com/eshop/services/CommerceService.java
    		changed "addToCartForUser" to check storage.

--- src/main/java/com/eshop/services/CommerceService.java
+++ src/main/java/com/eshop/services/CommerceService.java
@@ -3,7 +3,9 @@ package com.eshop.services;
 import com.eshop.dao.CartProductDAO;
 import com.eshop.dao.OrderDAO;
 import com.eshop.dao.OrderProductDAO;
+import com.eshop.dao.ProductDAO;
 import com.eshop.entities.*;
+import com.eshop.exceptions.InvalidProductQuantityException;
 import com.eshop.exceptions.ProductCartEmptyException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +27,9 @@ public class CommerceService {
     @Autowired
     private OrderDAO orderDAO;
 
+    @Autowired
+    private ProductDAO productDAO;
+
     public List<CartProduct> getCartProductsForUserById(Integer id){
         return cartProductDAO.findAllByUserId(id);
     }
@@ -34,9 +39,17 @@ public class CommerceService {
     }
 
 
-    public CartProduct addToCartForUser(User user, Product product, Integer quantity){
+    public CartProduct addToCartForUser(User user, Product product, Integer quantity)
+            throws InvalidProductQuantityException {
 
+        if(quantity > product.getQuantity()){
+            throw new InvalidProductQuantityException();
+        }
+        /*else{
+            product.setQuantity(product.getQuantity() - quantity);
 
+            productDAO.save(product);
+        }*/
 
         return cartProductDAO.save(new CartProduct(user, product, quantity));
     }
