40cb94e742b2d62613363c4b5f2c1412ad1a35a4
Karolis <incomirican@gmail.com>
2018-05-16 17:16:14 +0300

    blocked user are denied of access

--- src/main/java/com/eshop/services/LoginService.java
+++ src/main/java/com/eshop/services/LoginService.java
@@ -17,7 +17,7 @@ public class LoginService {
 
         try{
             User foundUser = userService.findByEmail(user.getEmail());
-            if(foundUser.getPassword() == user.getPassword()){
+            if(foundUser.getPassword() == user.getPassword() && !foundUser.isBlocked()){
                 return foundUser;
             }
             throw new InvalidUserCredentials();
