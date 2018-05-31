d6e2a5a43ea8c3f24dfafc736034ba1760ac0d91
Benas Orlovas <borlovas@gmail.com>
2018-05-31 11:24:35 +0300

    interceptors init

---
+++ src/main/java/com/eshop/controllers/interceptor/InterceptorConfig.java
@@ -0,0 +1,15 @@
+package com.eshop.controllers.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class InterceptorConfig extends WebMvcConfigurerAdapter  {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor());
+
+    }
+}
